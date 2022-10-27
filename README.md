3fae79ce125eddf874cb61ffc80d6fd742f53d84
stefano-mazziotta <mazziottastefano@gmail.com>
2022-10-27 23:08:57 +0200

    [dev] - first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# riverplatecircus-ecommerce
---
+++ wp-content/themes/riverplatecircus-theme/header.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package riverplatecircus-theme
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<div id="page" class="site">
+	
+	<header id="masthead" class="site-header">
+
+		<div class="header-wrap">
+			
+			<div class="header__logo"> 
+				<?php the_custom_logo(); ?>
+			</div>
+			
+			<nav id="site-navigation" class="header__nav">
+				<?php
+				wp_nav_menu(
+					array(
+						'theme_location' => 'menu-1',
+						'menu_id'        => 'primary-menu',
+					)
+				);
+				?>
+			</nav>
+		</div>
+	</header>
