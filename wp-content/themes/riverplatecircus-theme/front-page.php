de26750260096ff04d408ae9c8992ce50b0ce0c3
stefano-mazziotta <mazziottastefano@gmail.com>
2022-11-06 18:18:14 +0100

    [dev] home - hero layout

--- wp-content/themes/riverplatecircus-theme/front-page.php
+++ wp-content/themes/riverplatecircus-theme/front-page.php
@@ -13,9 +13,25 @@
  */
 
 get_header();
+// get_template_directory_uri
 ?>
 
 	<main id="primary" class="site-main">
+		<section class="hero">
+			<div class="hero__wrap">
+				<h1 class="title">River Plate Circus</h1>
+				<div class="content">
+					<h1 class="content__title">River Plate Circus</h1>
+					<p class="content__subtitle">Donde los sueños se hacen <strong>realidad</strong></p>
+					<a class="content__btn" href="">Compra tus entradas</a>
+				</div>
+				<div class="decoration">
+					<img class="blob" src="<?php echo get_template_directory_uri() ?>/img/svg/blobanimation.svg" alt="">
+					<img class="man" src="<?php echo get_template_directory_uri() ?>/img/png/colgado-1.png" alt="">
+				</div>
+			</div>
+		</section>
+
 
 
 	</main>
